7e5a4a76bb7eec9076d4037d0a336683b8f95524
YetAnotherNotHacking <yetanotherdingus@gmail.com>
2026-05-09 14:17:25 -0600

    attempt to stop browser from caching the images

--- app/app.py
+++ app/app.py
@@ -77,7 +77,7 @@ def create_app() -> Flask:
         if not os.path.exists(path):
             return Response(status=404)
         response = send_file(path, mimetype="image/png")
-        response.headers["Cache-Control"] = "public, max-age=31536000, immutable"
+        response.headers["Cache-Control"] = "public, max-age=60"
         return response
 
     @sock.route("/ws")
